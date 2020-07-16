4fc710258f16278bb484d9cf5b3c62f75a190503
Robert Cheng <bobcheng15@gmail.com>
2020-07-16 20:55:00 +0800

    models completed

---
+++ network.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+
+#################################################################
+#                           PARAMETERS                          #    
+#################################################################
+CONV1_FILTERS = 32
+CONV2_FILTERS = 64
+CONV3_FILTERS = 64
+
+#################################################################
+#                           ENCODER                             #    
+#################################################################
+
+class AutoEncoder_en():
+    def __init__(self):
+        #encoder
+        self.model = tf.keras.models.Sequential([
+            tf.keras.layers.Conv2D(CONV1_FILTERS, 8, [4, 4], activation=None),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Relu(),
+            tf.keras.layers.Conv2D(CONV1_FILTERS, 8, [4, 4], activation=None),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Relu(),
+            tf.keras.layers.Conv2D(CONV1_FILTERS, 8, [4, 4], activation=None),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Relu(), 
+        ])
+
+#################################################################
+#                           DECODER                             #    
+#################################################################
+
+class AutoEncoder_de():
+    def __init__(self):
+        self.model = tf.keras.models.Sequential([
+            tf.keras.layers.Conv2DTranspose(CONV1_FILTERS, 8, [4, 4], activation=None),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Relu(),
+            tf.keras.layers.Conv2DTranspose(CONV1_FILTERS, 8, [4, 4], activation=None),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Relu(),
+            tf.keras.layers.Conv2DTranspose(CONV1_FILTERS, 8, [4, 4], activation=None),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Relu(), 
+        ])
+
+class AutoEncoder():
+    def __init__(self):
+        self.encoder = AutoEncoder_en
+        self.decoder = AutoEncoder_de
+        self.model = tf.keras.models.Sequential([
+            self.encoder,
+            self.decoder,
+        ])
+
+AutoEncoder()
