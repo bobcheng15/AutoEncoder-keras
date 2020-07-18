7f0ec31d6bdea62b3e00386d8c112c66520fcb8a
Robert Cheng <bobcheng15@gmail.com>
2020-07-18 23:18:08 +0800

    fixes bug in tf.flag

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import tensorflow as tf
 #                           FLAGS                               #    
 #################################################################
 
-tf.app.flags.DEFINE_boolean("is_train", False, "training mode.")
-tf.app.flags.DEFINE_boolean("restore", False, "training mode.")
+tf.compat.v1.app.flags.DEFINE_boolean("is_train", False, "training mode.")
+tf.compat.v1.app.flags.DEFINE_boolean("restore", False, "training mode.")
 # ------- Training ------- #
-tf.app.flags.DEFINE_float("learning_rate", 0.001, "learning rate.")
-tf.app.flags.DEFINE_integer("epoch", 100, "Number of epoch.")
-tf.app.flags.DEFINE_integer("batch", 2000, "Number of images per batch.")
+tf.compat.v1.app.flags.DEFINE_float("learning_rate", 0.001, "learning rate.")
+tf.compat.v1.app.flags.DEFINE_integer("epoch", 100, "Number of epoch.")
+tf.compat.v1.app.flags.DEFINE_integer("batch", 2000, "Number of images per batch.")
 # ------ Save Model ------ #
-tf.app.flags.DEFINE_string("ckpt_dir", "./checkpoint", "check point directory")
-tf.app.flags.DEFINE_string("train_dir", "../Warehouse/atari_data/training", "Dataset directory")
-tf.app.flags.DEFINE_string("test_dir", "../Warehouse/atari_data/testing", "Testing directory")
-tf.app.flags.DEFINE_integer("inference_version", -1, "The version for inferencing.")
-tf.app.flags.DEFINE_integer("val_num", 16000, "number of validation images.")
+tf.compat.v1.app.flags.DEFINE_string("ckpt_dir", "./checkpoint", "check point directory")
+tf.compat.v1.app.flags.DEFINE_string("train_dir", "../Warehouse/atari_data/training", "Dataset directory")
+tf.compat.v1.app.flags.DEFINE_string("test_dir", "../Warehouse/atari_data/testing", "Testing directory")
+tf.compat.v1.app.flags.DEFINE_integer("inference_version", -1, "The version for inferencing.")
+tf.compat.v1.app.flags.DEFINE_integer("val_num", 16000, "number of validation images.")
 
 FLAGS = tf.app.flags.FLAGS
