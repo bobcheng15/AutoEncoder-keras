05e6392b26b3846863b36ab29535c93205669c35
Robert Cheng <bobcheng15@gmail.com>
2020-07-20 23:44:47 +0800

    all bugs fixed, runnable version. Some util funcs still needs to be completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import cv2
 import network
+import numpy as np
 
 #################################################################
 #                           FLAGS                               #    
@@ -11,49 +12,52 @@ tf.compat.v1.flags.DEFINE_boolean("restore", False, "training mode.")
 # ------- Training ------- #
 tf.compat.v1.flags.DEFINE_float("learning_rate", 0.001, "learning rate.")
 tf.compat.v1.flags.DEFINE_integer("epoch", 100, "Number of epoch.")
-tf.compat.v1.flags.DEFINE_integer("batch", 2000, "Number of images per batch.")
+tf.compat.v1.flags.DEFINE_integer("batch", 1000, "Number of images per batch.")
 # ------ Save Model ------ #
 tf.compat.v1.flags.DEFINE_string("ckpt_dir", "./checkpoint", "check point directory")
 tf.compat.v1.flags.DEFINE_string("train_dir", "../Warehouse/atari_data/training", "Dataset directory")
 tf.compat.v1.flags.DEFINE_string("test_dir", "../Warehouse/atari_data/testing", "Testing directory")
 tf.compat.v1.flags.DEFINE_integer("inference_version", -1, "The version for inferencing.")
-tf.compat.v1.flags.DEFINE_integer("val_num", 16000, "number of validation images.")
+tf.compat.v1.flags.DEFINE_integer("val_num", 10000, "number of validation images.")
 
 FLAGS = tf.compat.v1.flags.FLAGS
 
-def get_data(is_train=FLAGS.is_train):
+def get_data(is_train):
     output_img = [] # (4-d tensor) shape : size, w, h, 3
     if is_train:
         directory = FLAGS.train_dir
-        st, ed = 1, 50000
+        st, ed = 1, 20000
     else:
-        directory = FLAGS.test_dir
-        st, ed = 0, FALGS.val_num
+        directory = FLAGS.train_dir
+        st, ed = 1, FLAGS.val_num
 
     for i in range(st, ed+1):
         if i % 1000 == 0:
             print("loading {:d}-th img".format(i))
         path = directory + '/{:06d}.png'.format(i)
         img = cv2.imread(path)
-        cv2.resize(img, (84, 84))
+        img = cv2.resize(img, (84, 84))   
+        #print(img.shape)
         output_img.append(img/255.0)
+    output_img = np.array(output_img)
 
     return output_img
 
-def train(model, train_data, valid_data):
+def train(AE, train_data, valid_data):
     print("training...")
-    batch_loss = 0
-    st, ed, times = 0, 0, 0
-    max_len = len(data)
-    r = 0
-    for i in range (0, FLAG.epoch):
-        model.fit(data, data, FLAGS.batch, validation_data=(data, data), validation_batch_size=FLAGS.batch, verbose=1, return_dict=True)
-
+    for i in range (0, FLAGS.epoch):
+        start, end = 0, FLAGS.batch
+        while start < 20000:
+            AE.model.fit(train_data[start:end], train_data[start:end], batch_size=FLAGS.batch, verbose=1)
+            start += FLAGS.batch
+            end += FLAGS.batch
+        
 
 if __name__ == "__main__":
-    model = network.AutoEncoder()
-    train_data = get_data()
+    AE = network.AutoEncoder()
+    AE.init()
+    train_data = get_data(True)
     valid_data = get_data(False)
     if FLAGS.is_train:
-        train(model, train_data, valid_data)
+        train(AE, train_data, valid_data)
         
--- network.py
+++ network.py
@@ -15,13 +15,13 @@ class AutoEncoder_en():
     def __init__(self):
         #encoder
         self.model = tf.keras.models.Sequential([
-            tf.keras.layers.Conv2D(CONV1_FILTERS, 8, [4, 4], activation=None),
+            tf.keras.layers.Conv2D(CONV1_FILTERS, 8, [4, 4], padding="valid", activation=None),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.ReLU(),
-            tf.keras.layers.Conv2D(CONV1_FILTERS, 8, [4, 4], activation=None),
+            tf.keras.layers.Conv2D(CONV2_FILTERS, 4, [2, 2], padding="valid", activation=None),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.ReLU(),
-            tf.keras.layers.Conv2D(CONV1_FILTERS, 8, [4, 4], activation=None),
+            tf.keras.layers.Conv2D(CONV3_FILTERS, 3, [1, 1], padding="valid", activation=None),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.ReLU(), 
         ])
@@ -33,30 +33,33 @@ class AutoEncoder_en():
 class AutoEncoder_de():
     def __init__(self):
         self.model = tf.keras.models.Sequential([
-            tf.keras.layers.Conv2DTranspose(CONV1_FILTERS, 8, [4, 4], activation=None),
+            tf.keras.layers.Conv2DTranspose(CONV2_FILTERS, 3, [1, 1], padding="valid", activation=None),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.ReLU(),
-            tf.keras.layers.Conv2DTranspose(CONV1_FILTERS, 8, [4, 4], activation=None),
+            tf.keras.layers.Conv2DTranspose(CONV1_FILTERS, 4, [2, 2], padding="valid", activation=None),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.ReLU(),
-            tf.keras.layers.Conv2DTranspose(CONV1_FILTERS, 8, [4, 4], activation=None),
+            tf.keras.layers.Conv2DTranspose(3, 8, [4, 4], padding="valid", activation=None),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.ReLU(), 
         ])
 
-class AutoEncoder(lr = 0.001):
+class AutoEncoder():
+
     def __init__(self):
+        self.lr = 0.001
         self.encoder = AutoEncoder_en()
         self.decoder = AutoEncoder_de()
         self.model = tf.keras.models.Sequential([
             self.encoder.model,
             self.decoder.model,
         ])
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate=lr, beta_1=0.95)
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.lr, beta_1=0.95)
     
     def l2loss(self, y_true, y_pred):
+        tf.keras.backend.clip(y_pred, 0.0, 1.0)
         return (y_true - y_pred) ** 2
-
+    def init(self):
         self.model.compile(optimizer=self.optimizer, loss=self.l2loss, metrics=['accuracy'])
 
-AutoEncoder()
+#AutoEncoder()
