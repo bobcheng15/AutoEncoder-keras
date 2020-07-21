5634b532d24fca4642f66a4bde41f495770dbf49
Robert Cheng <bobcheng15@gmail.com>
2020-07-21 14:22:19 +0800

    bug fixes

--- main.py
+++ main.py
@@ -21,7 +21,12 @@ tf.compat.v1.flags.DEFINE_integer("inference_version", -1, "The version for infe
 tf.compat.v1.flags.DEFINE_integer("val_num", 10000, "number of validation images.")
 
 FLAGS = tf.compat.v1.flags.FLAGS
-
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
 def get_data(is_train):
     output_img = [] # (4-d tensor) shape : size, w, h, 3
     if is_train:
@@ -46,21 +51,21 @@ def get_data(is_train):
 def train(AE, train_data, valid_data):
     print("training...")
     for i in range (0, FLAGS.epoch):
-        start, end = 0, FLAGS.batch
-        if i % 100 == 0:
-            tf.keras.models.save_model(AE.encoder, "checkPoints/encoder", save_format="h5")
-            tf.keras.models.save_model(AE.decoder, "checkPoints/decoder", save_format="h5")
+        #start, end = 0, FLAGS.batch * 20
+        if i % 100 == 1 and i != 0:
+            tf.keras.models.save_model(AE.model, "checkPoints/encoder", save_format="h5")
+            #tf.keras.models.save_model(AE.decoder, "checkPoints/decoder", save_format="h5")
         while start < 20000:
-            AE.model.fit(train_data[start:end], train_data[start:end], batch_size=FLAGS.batch, verbose=1)
-            start += FLAGS.batch
-            end += FLAGS.batch
-        
-
+            AE.model.fit(train_data, train_data, batch_size=FLAGS.batch, verbose=1)
+            
 if __name__ == "__main__":
     AE = network.AutoEncoder()
     AE.init()
     train_data = get_data(True)
     valid_data = get_data(False)
+    if FLAGS.restore:
+        AE.encoder = tf.keras.models.load_model("checkpoints/encoder")
+        AE.decoder = tf.keras.models.load_model("checkpoints/decoder")
     if FLAGS.is_train:
         train(AE, train_data, valid_data)
         
