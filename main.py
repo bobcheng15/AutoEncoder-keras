0ef9f5f93677f79539a253e86833097abcd654c4
Robert Cheng <bobcheng15@gmail.com>
2020-07-22 02:31:44 +0800

    AE training completed

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ tf.compat.v1.flags.DEFINE_boolean("restore", False, "training mode.")
 tf.compat.v1.flags.DEFINE_float("learning_rate", 0.001, "learning rate.")
 tf.compat.v1.flags.DEFINE_integer("epoch", 50, "Number of epoch.")
 tf.compat.v1.flags.DEFINE_integer("batch", 1000, "Number of images per batch.")
+tf.compat.v1.flags.DEFINE_integer("start_index", 1, "The first image's index")
 # ------ Save Model ------ #
 tf.compat.v1.flags.DEFINE_string("ckpt_dir", "./checkpoint", "check point directory")
-tf.compat.v1.flags.DEFINE_string("train_dir", "../Warehouse/atari_data/training", "Dataset directory")
+tf.compat.v1.flags.DEFINE_string("train_dir", "../training", "Dataset directory")
 tf.compat.v1.flags.DEFINE_string("test_dir", "../Warehouse/atari_data/testing", "Testing directory")
 tf.compat.v1.flags.DEFINE_integer("inference_version", -1, "The version for inferencing.")
 tf.compat.v1.flags.DEFINE_integer("val_num", 10000, "number of validation images.")
@@ -25,10 +26,10 @@ def get_data(is_train):
     output_img = [] # (4-d tensor) shape : size, w, h, 3
     if is_train:
         directory = FLAGS.train_dir
-        st, ed = 1, 20000
+        st, ed = FLAGS.start_index, FLAGS.start_index + 19999
     else:
         directory = FLAGS.train_dir
-        st, ed = 20001, 20000 + FLAGS.val_num
+        st, ed = FLAGS.start_index + 20000, FLAGS.start_index + 29999
 
     for i in range(st, ed+1):
         if i % 1000 == 0:
@@ -44,13 +45,12 @@ def get_data(is_train):
 
 def train(AE, train_data, valid_data):
     print("training...")
-    for i in range (0, FLAGS.epoch):
+    for i in range (0, int(FLAGS.epoch/10)):
         #start, end = 0, FLAGS.batch * 20i
-        if i % 10 == 0:
-            validate(AE, valid_data)
-        AE.model.save("checkPoint/model.h5")    
+        validate(AE, valid_data)
+        #AE.model.save("checkPoint/AE.h5")    
         #tf.keras.models.save_model(AE.decoder, "checkPoints/decoder", save_format="h5")
-        AE.model.fit(train_data, train_data, batch_size=FLAGS.batch, verbose=1)
+        AE.model.fit(train_data, train_data, epochs=10, batch_size=FLAGS.batch, verbose=1)
 
 def validate(AE, valid_data):
     print("validating...")
@@ -64,10 +64,24 @@ if __name__ == "__main__":
     train_data = get_data(True)
     valid_data = get_data(False)
     if FLAGS.restore:
-        loaded = tf.keras.models.load_model("./checkPoint/model.h5", compile=False)
-        loaded.layers.pop()
+        loaded = tf.keras.models.load_model("./checkPoint/AE.h5", compile=False)
+        #use this line of code to only load the encoder's weight
+        #AE.model.layers[0].set_weights(loaded.layers[0].get_weights())
         AE.model.set_weights(loaded.get_weights())
     if FLAGS.is_train:
         train(AE, train_data, valid_data)
-        
+    else:
+        input_img = []
+        img = cv2.imread(FLAGS.train_dir + "/000001.png")
+        img = cv2.resize(img, (84, 84))
+        print(img.shape)
+        img = img / 255.0
+        input_img.append(img)
+        input_img = np.array(input_img)
+        result = AE.model.predict(input_img)
+        result = result * 255
+        result.astype(int)
+        print(result.shape)
+        cv2.imwrite("fake.png", result[0,:,:,:])
+
 
---
+++ train.sh
@@ -0,0 +1,15 @@
+python3 main.py --is_train=True --start_index=1
+python3 main.py --is_train=True --start_index=30001 --restore=True
+python3 main.py --is_train=True --start_index=30001 --restore=True
+python3 main.py --is_train=True --start_index=60001 --restore=True
+python3 main.py --is_train=True --start_index=60001 --restore=True
+python3 main.py --is_train=True --start_index=90001 --restore=True
+python3 main.py --is_train=True --start_index=90001 --restore=True
+python3 main.py --is_train=True --start_index=120001 --restore=True
+python3 main.py --is_train=True --start_index=120001 --restore=True
+python3 main.py --is_train=True --start_index=150001 --restore=True
+python3 main.py --is_train=True --start_index=150001 --restore=True
+python3 main.py --is_train=True --start_index=180001 --restore=True
+python3 main.py --is_train=True --start_index=180001 --restore=True
+python3 main.py --is_train=True --start_index=210001 --restore=True
+python3 main.py --is_train=True --start_index=210001 --restore=True
